ed8160088597bc071b4e2b4ccb0727467a7ead80
Michelet <michelet@usp.br>
2021-10-06 21:05:51 -0300

    dev

--- pages/components/Todo.js
+++ pages/components/Todo.js
@@ -1,11 +1,17 @@
 export default function Todo({ todo, index, completeTodo, removeTodo }) {
+  let isCompleted = false
+  let text = ''
+  if (todo) {
+    isCompleted = todo.isCompleted
+    text = todo.text
+  }
   return (
     <div
       className="flex flex-row justify-start px-2 text-lg"
       id={index}
-      style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
+      style={{ textDecoration: isCompleted ? 'line-through' : '' }}
     >
-      <div>{todo.text}</div>
+      <div>{text}</div>
       <div>
         <button
           className="font-bold text-green-800 "
