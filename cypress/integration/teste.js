fb0c86fa4ef25f7a4a9490460ad547138bcd2e86
Michelet <michelet@usp.br>
2021-10-07 13:37:21 -0300

    tes

--- cypress/integration/teste.js
+++ cypress/integration/teste.js
@@ -1,20 +1,23 @@
 describe('Navegação e Funcionalidade', () => {
+  /** */
   it('deve carregar a página', () => {
     cy.visit('http://localhost:3000/')
   })
+  /** */
   it('deve ter 3 elementos iniciais', () => {
     cy.get('[id="listaTodos"]').children().should('have.length', 3)
   })
-  it('deve incluir uma tarefa nova', () => {
-    cy.get('[id="formulario_todo"]')
-      .type('alguma tarefa nova')
-      .should('have.value', 'alguma tarefa nova')
+  /** */
+  it('deve preencher o formulário e incluir "Tarefa Nova"', () => {
+    const tarefa = 'Tarefa Nova'
+    cy.get('[id="formulario_todo"]').type(tarefa).should('have.value', tarefa)
     cy.get('[id="formulario_todo_button"]').click()
   })
-
+  /** */
   it('deve conter 4 elementos após a inclusão', () => {
     cy.get('[id="listaTodos"]').children().should('have.length', 4)
   })
+  /** */
   it('deve eliminar a primeira tarefa', () => {
     cy.get('[id="listaTodos"]')
       .children()
@@ -25,6 +28,7 @@ describe('Navegação e Funcionalidade', () => {
       .last()
       .click()
   })
+  /** */
   it('deve ter três elementos após a eliminação do primeiro', () => {
     cy.get('[id="listaTodos"]').children().should('have.length', 3)
   })
