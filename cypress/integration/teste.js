366b51103b7c0e68d9ab963a38e746815a99744e
michelet <michelet@usp.br>
2021-12-06 11:22:34 -0300

    updating cypress tests.

--- cypress/integration/teste.js
+++ cypress/integration/teste.js
@@ -1,12 +1,35 @@
-describe('Navegação e Funcionalidade', () => {
+describe('Casos de Testes de Funcionalidade', () => {
   /** */
   it('deve carregar a página', () => {
     cy.visit('http://localhost:3000/')
   })
-  /** */
-  it('deve ter 3 elementos iniciais', () => {
+  // Conteúdo
+  it('deve mostrar "Mais uma Todo..." na aba principal', () => {
+    cy.title().should('eq', 'Mais uma Todo...')
+  })
+  it('deve mostrar "Mais uma Todo..." no como título da página', () => {
+    cy.get('h1').should('have.text', 'Mais uma Todo...')
+  })
+
+  it('deve ter 3 tarefas iniciais', () => {
     cy.get('[id="listaTodos"]').children().should('have.length', 3)
   })
+  //Estética
+  it('deve mostrar o título na familia de fontes mono space, que contem a fonte Courier New', () => {
+    cy.get('h1')
+      .should('have.css', 'font-family')
+      .and('match', /Courier/)
+  })
+
+  it('deve mostrar o título com uma largura de fonte 700 (negrito)', () => {
+    cy.get('h1').should('have.css', 'font-weight', '700')
+  })
+  it('deve mostrar o título com background em amarelo', () => {
+    cy.get('h1').should('have.css', 'background-color', 'rgb(253, 230, 138)')
+  })
+
+  // Interface
+
   /** */
   it('deve preencher o formulário e incluir "Tarefa Nova"', () => {
     const tarefa = 'Tarefa Nova'
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Todo from './components/Todo'
 import TodoForm from './components/TodoForm'
 import { useState } from 'react'
+import Head from 'next/head'
 import uuid from 'react-uuid'
 function App() {
   const [todos, setTodos] = useState([
@@ -36,46 +37,52 @@ function App() {
   }
 
   return (
-    <div className="container px-4 py-4 mx-auto text-center">
-      <div className="flex flex-col ">
-        <h1 className="text-2xl font-bold text-center ">
-          Mais uma "todo list..."
-        </h1>
+    <>
+      <Head>
+        <title>Mais uma Todo...</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="container px-4 py-4 mx-auto text-center">
+        <div className="flex flex-col ">
+          <h1 className="font-mono text-2xl font-bold text-center bg-yellow-200">
+            Mais uma Todo...
+          </h1>
 
-        <ul className="flex-col p-3">
-          <li>Digite alguma tarefa no quadrado amarelo e aperte enter.</li>
-          <li>Clique no ✔ indicar uma tarefa feita</li>
-          <li>Clique no &#10060;para remover uma tarefa</li>
-        </ul>
-      </div>
+          <ul className="flex-col p-3">
+            <li>Digite alguma tarefa no quadrado amarelo e aperte enter.</li>
+            <li>Clique no ✔ indicar uma tarefa feita</li>
+            <li>Clique no &#10060;para remover uma tarefa</li>
+          </ul>
+        </div>
 
-      <div className="flex flex-col py-2 ">
-        {todos.length > 0 ? (
-          <div id="tituloLista" className="text-lg font-semibold">
-            A seguir a Lista de Tarefas...
+        <div className="flex flex-col py-2 ">
+          {todos.length > 0 ? (
+            <div id="tituloLista" className="text-lg font-semibold">
+              A seguir a Lista de Tarefas...
+            </div>
+          ) : (
+            <div id="listaVazia" className="text-lg font-semibold text-red-800">
+              a Todo-List está vazia, adicione uma tarefa...
+            </div>
+          )}
+          <div className="flex flex-col items-center py-2" id={'listaTodos'}>
+            {todos.map((todo, index) => (
+              <Todo
+                key={uuid()}
+                id={uuid()}
+                index={index}
+                todo={todo}
+                completeTodo={completeTodo}
+                removeTodo={removeTodo}
+              />
+            ))}
           </div>
-        ) : (
-          <div id="listaVazia" className="text-lg font-semibold text-red-800">
-            a Todo-List está vazia, adicione uma tarefa...
+          <div className="py-2">
+            <TodoForm addTodo={addTodo} />
           </div>
-        )}
-        <div className="flex flex-col items-center py-2" id={'listaTodos'}>
-          {todos.map((todo, index) => (
-            <Todo
-              key={uuid()}
-              id={uuid()}
-              index={index}
-              todo={todo}
-              completeTodo={completeTodo}
-              removeTodo={removeTodo}
-            />
-          ))}
-        </div>
-        <div className="py-2">
-          <TodoForm addTodo={addTodo} />
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
